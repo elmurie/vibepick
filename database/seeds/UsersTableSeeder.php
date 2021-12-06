2e9f9d03f76ef6f98ba78a15e9e9513947bcc33f
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-12-06 16:57:07 +0100

    modificato seeder utente

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,13 +15,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        // tabella per poi mettere utenti piú reali
+        $utenti = [
+            [
+                'firstname' => '',
+                'lastname' => '',
+                'address' => '',
+                'phone_number' => '',
+            ],
+        ];
         for ($i=0; $i<20; $i++) {
             $newUser = new User();
             $newUser->firstname = $faker->firstName(); 
             $newUser->lastname = $faker->lastName();
             $newUser->address = $faker->streetAddress();
-            $newUser->profile_pic = $faker->imageUrl();
-            $newUser->phone_number = $faker->randomNumber(9, true);
+            $newUser->phone_number = 3 . $faker->randomNumber(9, true);
             $newUser->email = $faker->email();
             $newUser->password = Hash::make("prova1234");
             $newUser->services = $faker->word();
