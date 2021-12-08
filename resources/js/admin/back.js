bce278ce7993942ec531ce9059ce6d6ba6c6e99c
elmurie <elmurie@gmail.com>
2021-12-08 11:27:22 +0100

    refactoring script backoffice

--- resources/js/admin/back.js
+++ resources/js/admin/back.js
@@ -1,7 +1,32 @@
+//Questa funzione verifica che sia stata selezionata almeno una checkbox agendo sull'attributo required del tag input
+
+deRequireCb = function(elClass) {
+    el=document.getElementsByClassName(elClass);
+
+    var atLeastOneChecked=false;//Almeno una checkbox è stata selezionata
+    for (i=0; i<el.length; i++) {
+        if (el[i].checked === true) {
+            atLeastOneChecked=true;
+        }
+    }
+
+    if (atLeastOneChecked === true) {
+        for (i=0; i<el.length; i++) {
+            el[i].required = false;
+        }
+    } else {
+        for (i=0; i<el.length; i++) {
+            el[i].required = true;
+        }
+    }
+}
+
+// Overlay toast aggiornamento profilo
+
 const toast = document.getElementById('toast__vibe');
 const toastBg = document.getElementById('bg__toast__vibe');
 
 opacityToast = function() {
     toast.style.display = 'none'
     toastBg.style.display = 'none'
-} 
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/checkBoxValidation.js', 'public/js')
     .js('resources/js/guest/front.js', 'public/js')
     .js('resources/js/admin/back.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
