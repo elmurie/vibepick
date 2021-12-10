fcf58baa097c90e077b3373e2d662cc784562950
MarioFantasia <mariofantasia18@outlook.it>
2021-12-10 19:10:43 +0100

    rimozione test fetch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,6 @@
                 {{-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a> --}}
-                <div>Test Fetch</div>
                 <a href="{{url('/')}}"> Home</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
