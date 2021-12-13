8b8a44e63dabbde051740d159f10b40227f04a5b
Kio-dev-it <pas.petito@gmail.com>
2021-12-13 16:15:33 +0100

    Filtro eloquent e php per num rev e media voto

--- app/Http/Controllers/Api/InstrumentController.php
+++ app/Http/Controllers/Api/InstrumentController.php
@@ -25,33 +25,8 @@ class InstrumentController extends Controller
     public function show($slug, $rewMin, $avgVote)
     {
 
-        //Chiamata sugli utenti 
-        
-        // $users = User::with('instruments','reviews')->get();
-        // $users = User::with('instruments')->where('instruments_id', 1)->get();
-        // $users = Instrument::with('users')->where('slug',$slug)->get();
-            // $users = Review::with('users')->where('vote', '>', 2)->get();
 
-            // $users = User::find(1)->reviews;
-
-        //Questa prende gli utenti che suonano lo strumento $slug
-            // $users = Instrument::where('slug', $slug)->with('users')->get();
-        
-        //Questa richiama gli utenti che hanno recensioni con voti maggioni di un valore e porta con se le reviews
-            // $users = User::whereHas('reviews', function (Builder $query){
-            //     $query->where('vote', '>', 4);
-            // })->with('reviews')->get();
-
-    // Questa richiama gli utenti che suonano quello strumento
-        // $users = User::whereHas('instruments', function (Builder $query) use($slug){
-        //     $query->where('slug', $slug);
-        // })
-        // ->withCount('reviews')->having('reviews_count', '>=', $rewMin)
-        // ->whereHas('reviews', function (Builder $query) use ($avgVote){
-        //     $query->where('user_id' ,1);
-        // })->with('reviews')->get();
-
-                //eseguo la chiamata su user selezionando in base allo strumento
+        //eseguo la chiamata su user selezionando in base allo strumento
         $users = User::whereHas('instruments', function (Builder $query) use($slug){
             $query->where('slug', $slug);
         })
@@ -78,43 +53,13 @@ class InstrumentController extends Controller
                 }
                 //eseguo il calcolo della media
                 $averageVote = $totVote/$user['reviews_count'];
+                $user['avgVote'] = round($averageVote, 1);
             }
             //se la media è maggiore di quella richiesta inietto l'utente nell'array che fungerà da risposta
             if( $averageVote >= $avgVote){
                 $usersFiltered[] = $user;
             }
         }
-
-                // foreach($users as $user){
-                //     $totVote = 0;
-                //     $reviewTot=0;
-                //     foreach($user['instruments'] as $instrument){
-                //         if($instrument['slug'] == $slug){
-                //             if(count($user['reviews']) > 0){
-                //                 $reviewTot = count($user['reviews']); 
-                //             }
-                            
-                //             // dd($reviewTot);
-                //             if($reviewTot >= $rewMin){
-                //                 foreach($user['reviews'] as $review){
-                //                     // dd($review['vote']);
-                //                     $totVote += $review['vote'];
-                //                 }
-                //                 if($reviewTot > 0){
-                //                     $averageVote = $totVote / $reviewTot;
-                //                 } else{
-                //                     $averageVote = 0;
-                //                 }
-                //                 if( $averageVote >= $avgVote){
-                //                     $usersFiltered[] = $user;
-                //                 }
-                //             }
-
-                //         }
-                //         }
-                //     }
-                    
-                    // dd($usersFiltered);
         return response()->json([
             'success' => true,
             'data' => $usersFiltered,
