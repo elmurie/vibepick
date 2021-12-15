d5ed18ebe2aff1a245206c649e8d1a7ef05708ae
elmurie <elmurie@gmail.com>
2021-12-15 23:40:09 +0100

    aggiunto copyright footer

--- resources/views/layouts/sponsor.blade.php
+++ resources/views/layouts/sponsor.blade.php
@@ -98,6 +98,11 @@
             <main>
                 @yield('content')
             </main>
+            <footer>
+                <div>
+                    | Copyright &copy; VibePick 2021
+                </div>
+            </footer>
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js" integrity="sha512-ubuT8Z88WxezgSqf3RLuNi5lmjstiJcyezx34yIU2gAHonIi27Na7atqzUZCOoY4CExaoFumzOsFQ2Ch+I/HCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -110,9 +115,6 @@
                     toastr.success("{!!Session::get('record_updated')!!}");
                 </script>
         @endif
-            <footer>
-                
-            </footer>
             {{-- Braintree script --}}
             <script src="https://js.braintreegateway.com/web/dropin/1.13.0/js/dropin.min.js"></script>
             <script>
