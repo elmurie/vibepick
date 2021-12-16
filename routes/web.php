e0725bcedb52d4ae964fc3f1ed937ec248d72f35
Kio-dev-it <pas.petito@gmail.com>
2021-12-16 18:16:17 +0100

    Agg gest transazioni e rit a pagina sponsor index

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Braintree\Gateway;
 
 use App\User;
 use App\Sponsorship;
+use Illuminate\Database\Eloquent\Builder;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -75,6 +77,16 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
     
     Route::post('/checkout', function(Request $request, User $user){
 
+        //Trasformazione delle date in oggetti DateTime per sfruttare la maggior precisione nei confronti 
+        $nextSponsorStart = new DateTime($request->start_time);
+        
+        date_default_timezone_set('Europe/Rome');
+        $now = date("Y-m-d H:i:s");
+        $nowDate = new DateTime($now);
+
+        if($nowDate > $nextSponsorStart){
+            return back()->withErrors('Non puoi selezionare una data di inizio sponsorizzazione precedente al tempo attuale');
+        }
         
         if($user = Auth::user()){
             $gateway = new Braintree\Gateway([
@@ -88,6 +100,25 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
             $sponsorship = Sponsorship::find($request->sponsor_id);
             $amount = $sponsorship->price;
             $nonce = $request->payment_method_nonce;
+
+    //La chiamata restituisce l'oggetto con l'utente e le sponsorships a lui legate 
+            $loggedUser = User::where('id', $user->id)->with('sponsorships')->get();
+
+    //Con questa assegnazione si accede all'array di sponsorship legate all'utente
+            $userSponsorships = $loggedUser[0]['sponsorships'];
+        
+    //In questo ciclo confrontiamo il tempo di fine delle sponsorship legate all'user con la nuova che vuole attivare 
+            foreach($userSponsorships as $userSponsorship){
+            
+            //Trasformazione delle date in oggetti DateTime per sfruttare la maggior precisione nei confronti 
+                $prevSponsorEnd = new DateTime($userSponsorship['pivot']['end_time']);
+
+                //Se la nuova sponsorizzazione ha uno start_time all'interno di un periodo di sponsorizzazione attiva si torna alla pagina con un errore
+                if($prevSponsorEnd > $nextSponsorStart){
+                    return back()->withErrors('Hai una sponsorizzazione in corso per la data di inizio selezionata, si conclude il: ' . date_format($prevSponsorEnd , 'd-m-Y') . ' alle ' . date_format($prevSponsorEnd , 'H:i:s'));
+                }
+            }
+
         
             $result = $gateway->transaction()->sale([
                 'amount' => $amount,
@@ -104,7 +135,6 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
         
             if ($result->success) {
                 $transaction = $result->transaction;
-                // header("Location: transaction.php?id=" . $transaction->id);
 
                 // inserimento manuale del created_at per avere disponibile uno stroico privato di attivazione della sponsorship
                 date_default_timezone_set('Europe/Rome');
@@ -116,6 +146,7 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
                 //Aggiungiamo la durata della sponsor in giorni
                 $end_date = date_add($start_date, date_interval_create_from_date_string($sponsorship->duration . " days"));
                 // $request->start_time + $sponsorship->duration;
+
                 //Questo array prende i dati di inizio e fine della sponsor e li raggruppa
                 $sponsor_dati = [
                     'start_time' => $request->start_time,
@@ -126,7 +157,9 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
     
                 //grazie all'attach colleghiamo user_id e sponsorship_id e aggiungiamo i dati ulteriori sulla durata nella tabella pivot sponsorship_user
                 $user->sponsorships()->attach($request->sponsor_id, $sponsor_dati );
-                return back()->with('success_message', 'Transazione riuscita. The ID is:'. $transaction->id);
+                $startHour = date_format($nextSponsorStart, "H:i:s");
+                $startDay = date_format($nextSponsorStart, "d-m-Y");
+                return redirect()->route('admin.sponsorship')->with('success_message', 'Acquisto completato. Avrai la sponsor ' . $sponsorship->name . ' per ' . $sponsorship->duration*24 . 'ore. A partire dalle ' . $startHour . ' del ' . $startDay . '. Identificativo della transazione: '. $transaction->id);
             } else {
                 $errorString = "";
         
