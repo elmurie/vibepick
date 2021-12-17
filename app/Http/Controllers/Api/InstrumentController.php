fc5e8cf2524f5d8a68e3c89f1e897be0e2c3d7ef
Kio-dev-it <pas.petito@gmail.com>
2021-12-17 19:31:10 +0100

    Perfezionamento chiamata advanced search

--- app/Http/Controllers/Api/InstrumentController.php
+++ app/Http/Controllers/Api/InstrumentController.php
@@ -22,64 +22,37 @@ class InstrumentController extends Controller
         ]);
     }
 
+    // ----------- CHIAMATE PER LA RICERCA UTENTI IN ADVANCED SEARCH -----------
+
     public function show($slug, $rewMin, $avgVote)
     {
 
-            // date_default_timezone_set('Europe/Rome');
-            // $nowDate = date("Y-m-d H:i:s");
-            // $prova = "2021-12-23 10:44:00";
-            // $cond = $prova > $nowDate;
-            // dd($cond);
-        //eseguo la chiamata su user selezionando in base allo strumento
-        // $usersSponsor = User::whereHas('instruments', function (Builder $query) use($slug){
-        //     $query->where('slug', $slug);
-        // })->with('sponsorships')
-        // ->whereHas('sponsorships', function(Builder $query){
-        //     // date_default_timezone_set('Europe/Rome');
-        //     // $nowDate = date("Y-m-d H:i:s");
-            // $nowDate = date("2021-12-17 12:32:00");
-        //     // dd($nowDate);
-        //     $query->where('end_time', '>', $nowDate)->where('start_time', '<', $nowDate);
-        // })
-        // //conto il numero di review
-        // ->withCount('reviews')
-        // //imposto la condizione sul numero di review
-        // ->having('reviews_count', '>=', $rewMin)
-        // //collego tutte le review perché sono una pippa e devo usarle per applicare la condizione sulla media voto
-        // ->with('reviews')->get();
+            date_default_timezone_set('Europe/Rome');
+            $nowDate = date("Y-m-d H:i:s");
 
-//-----------------Così la not sponsor funziona con quelli che hanno una sola sponsor ma che non è attiva (scaduta o attivazione futura)
 
-        // $usersNotSponsor = User::whereHas('instruments', function (Builder $query) use($slug){
-        //     $query->where('slug', $slug);
-        // })
-        // // ->doesntHave('sponsorships')
-        // ->whereHas('sponsorships', function(Builder $query){
-        //     // date_default_timezone_set('Europe/Rome');
-        //     // $nowDate = date("Y-m-d H:i:s");
-        //     $nowDate = date("2021-12-17 12:32:00");
-        //     // dd($nowDate);
-        //     $query->where('end_time', '<', $nowDate)->whereOr('start_time', '<', $nowDate);
-        // })
-        // //conto il numero di review
-        // ->withCount('reviews')
-        // //imposto la condizione sul numero di review
-        // ->having('reviews_count', '>=', $rewMin)
-        // //collego tutte le review perché sono una pippa e devo usarle per applicare la condizione sulla media voto
-        // ->with('reviews')->get();
+
+//----------------- Chiamata per gli utenti che hanno una sponsorizzazione attiva
+        $usersSponsor = User::whereHas('instruments', function (Builder $query) use($slug){
+            $query->where('slug', $slug);
+        })
+        ->whereHas('sponsorships', function(Builder $query) use($nowDate){
+            $query->where('end_time', '>', $nowDate)->where('start_time', '<', $nowDate);
+        })
+        //Per contare il numero di review
+        ->withCount('reviews')
+        //Richiesta in base alla condizione sul numero di review
+        ->having('reviews_count', '>=', $rewMin)
+        //collego tutte le review perché sono una pippa e devo usarle per applicare la condizione sulla media voto
+        ->with('reviews')->get();
 
 
 //------------Chiamata per utenti senza sponsor attive ma che ne hanno almeno una acquistata (scaduta o con attivazione futura)
-        $usersNotSponsor = User::whereHas('instruments', function (Builder $query) use($slug){
+        $usersNotActiveSponsor = User::whereHas('instruments', function (Builder $query) use($slug){
             $query->where('slug', $slug);
         })
-        // ->doesntHave('sponsorships')
-        ->whereHas('sponsorships', function(Builder $query){
-            // date_default_timezone_set('Europe/Rome');
-            // $nowDate = date("Y-m-d H:i:s");
-            $nowDate = date("2021-12-25 12:32:00");
-            // dd($nowDate);
-            $query->where('end_time', '<', $nowDate)->whereOr('start_time', '<', $nowDate);
+        ->whereHas('sponsorships', function(Builder $query) use($nowDate){
+            $query->where('end_time', '<', $nowDate)->orWhere('start_time', '<', $nowDate);
         })
         //conto il numero di review
         ->withCount('reviews')
@@ -88,20 +61,19 @@ class InstrumentController extends Controller
         //collego tutte le review perché sono una pippa e devo usarle per applicare la condizione sulla media voto
         ->with('reviews')->get();
 
-        $notSponsorLength = count($usersNotSponsor);
-
-        //l'array delle persone con delle sponsor non attive ha qualche dato?
+        $notSponsorLength = count($usersNotActiveSponsor);
+        
+        //L'array delle persone con delle sponsor non attive ha qualche dato?
         if($notSponsorLength > 0){
-            $nowDate = date("2021-12-25 12:32:00");
 
             //Per ogni elemento dell'array di persone con sponsor non attive controlliamo se ne ha anche una attiva
             for($i =  0; $i < $notSponsorLength; $i++){
                 
                 //variabile di supporto per la verifica dell'eventuale sponsor attiva
                 $isNowSponsored = false;
-
+                
                 //ciclo su tutte le sponsorizzazioni legate al singolo utente
-                foreach($usersNotSponsor[$i]['sponsorships'] as $sponsorship ){
+                foreach($usersNotActiveSponsor[$i]['sponsorships'] as $sponsorship ){
                     
                     //Se una delle sponsor legate all'utente comprende il giorno attuale
                     if($sponsorship['pivot']['end_time'] > $nowDate && $sponsorship['pivot']['start_time'] < $nowDate){
@@ -114,39 +86,66 @@ class InstrumentController extends Controller
 
                 //Se la variabile di supporto è true l'elemento dell'array collegato all'utente viene cancellato
                 if($isNowSponsored){
-                    unset($usersNotSponsor[$i]);
+                    unset($usersNotActiveSponsor[$i]);
                 }
             }
         }
 
-// ----------------- Ci sarebbe bisogno della chiamata per tutti quelli che non hanno mai attivato sponsor usando il doesntHave('sponsorship')
+// ----------------- Chiamata per gli utenti che non hanno mai attivato una sponsorizzazione
 
+        $usersNotSponsor = User::whereHas('instruments', function (Builder $query) use($slug){
+            $query->where('slug', $slug);
+        })->doesntHave('sponsorships')
+        //conto il numero di review
+        ->withCount('reviews')
+        //imposto la condizione sul numero di review
+        ->having('reviews_count', '>=', $rewMin)
+        //collego tutte le review perché sono una pippa e devo usarle per applicare la condizione sulla media voto
+        ->with('reviews')->get();
+
+        //Definizione di un array vuoto che sarà di supporto per la risposta dell'API
         $users = [];
-        // foreach ($usersSponsor as $sponsor) {
-        //     $users[]=$sponsor;
-        // }
-        foreach ($usersNotSponsor as $sponsor) {
-        $users[]=$sponsor;
+
+    //Riempimento risposta con gli utenti che rispettano i criteri di ricerca
+        //Ciclo utenti hanno una sponsorizzazione attiva
+        foreach ($usersSponsor as $sponsor) {
+            $users[]=$sponsor;
         }
 
-        //setto vuoto l'array di risposta
-        $usersFiltered = [];
-        foreach($users as $user){
-            //Setto a 0 il totale dei voti e la media
+        //Ciclo utenti che hanno almeno una sponsorizzazione non attiva
+        foreach ($usersNotActiveSponsor as $sponsor) {
+            $users[]=$sponsor;
+        }
 
+        //Ciclo utenti che non hanno mai attivato una sponsorizzazione
+        foreach ($usersNotSponsor as $sponsor) {
+            $users[]=$sponsor;
+        }
+        
+        //Settaggio vuoto l'array di risposta dell'API
+        $usersFiltered = [];
+
+        //Ciclo per riempire l'array di risposta con il dato aggiuntivo della media voto se la condizione su quest'ultima è verificato
+        foreach($users as $user){
+
+            //Si setta a 0 il totale dei voti e la media
             $totVote = 0;
             $averageVote = 0;
             $user['avgVote'] = 0;
-            //il reviews_count ci viene passato dalla query, entriamo nell'if solo se c'è alemeno una review
+
+            //Il reviews_count viene passato dalla query, si entra nell'if solo se c'è alemeno una review
             if($user['reviews_count'] > 0){
+
                 //ciclo su tutte le review dell'utente per ottenere la somma voti
                 foreach($user['reviews'] as $review){
                     $totVote += $review['vote'];
                 }
+
                 //eseguo il calcolo della media
                 $averageVote = $totVote/$user['reviews_count'];
                 $user['avgVote'] = round($averageVote, 1);
             }
+
             //se la media è maggiore di quella richiesta inietto l'utente nell'array che fungerà da risposta
             if( $averageVote >= $avgVote){
                 $usersFiltered[] = $user;
