d259d834c34b851e5b64f72859a6df31b582a1d6
MarioFantasia <mariofantasia18@outlook.it>
2021-12-17 01:50:42 +0100

    modifica seeder messaggi e recensioni

--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -14,16 +14,83 @@ class MessagesTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //Riempimento tabella messaggi tramite Faker
-        //ToDo: creare un array di messaggi verosimili o istruire meglio il faker
-        for ( $i = 0; $i < 40; $i++ ) {
+        //array per compilare tabella messaggi
+
+
+
+        //compilazione tabella messaggi
+        for ($i = 0; $i <40; $i++) {
+            //contatori array
+            $separator = [
+                '.',
+                '_'
+            ];
+    
+            $emailDom = [
+                '@gmail.it',
+                '@gmail.com',
+                '@outlook.it',
+                '@outlook.com',
+                '@hotmail.it',
+                '@hotmail.com',
+                '@live.it',
+                '@live.com',
+            ];
+            
+            $firstGreeting = [
+                'Buongiorno',
+                'Salve',
+                'Buonasera',
+            ];
+
+            $firstReason = [
+                'sapere se è disponibile per una collaborazione',
+                'sapere è disponibile per una data del mio tour',
+                'avere informazioni riguardo i prezzi per una serata',
+                'chiederle in che giorni è disponibile per avere delle lezioni',
+            ];
+
+            $secondReason = [
+                'il suo tariffario',
+                "se è possibile fissare un incontro per approfondire l'argomento",
+                'se le interessa registrare nel mio studio'
+            ];
+
+            $secondGreeting = [
+                'Saluti',
+                'A presto',
+                'Attendo sue notizie'
+            ];
+
+            //contatori array
+            $countSeparator=array_rand($separator, 1);
+            $countDom=array_rand($emailDom, 1);
+            $countFirstG=array_rand($firstGreeting, 1);
+            $countFirstR=array_rand($firstReason, 1);
+            $countSecondG=array_rand($secondGreeting, 1);
+            $countSecondR=array_rand($secondReason, 1);
+
+
+            //variabili seeder
+            $varUser_id =$faker->numberBetween(1, 25);
+            $varFirstname=$faker->firstName();
+            $varLastname=$faker->lastName();
+            $varEmail= strtolower($varFirstname . $separator[$countSeparator] . $varLastname . $emailDom[$countDom]);
+            
+            $varTxt1 = $firstGreeting[$countFirstG] . '. La contatto per ' . $firstReason[$countFirstR];
+            $varText2 = '. Vorrei sapere inoltre ' . $secondReason[$countSecondR] . '. '. $secondGreeting[$countSecondG];
+            $varText = $varTxt1 . $varText2 . '.';
+            
+            //SEEDER
             $newMessage = new Message();
-            $newMessage->user_id = $faker->numberBetween(1, 20); //generazione casuale della foreign key in modo da avere un assegnazione per gli user creati con il seeder
-            $newMessage->firstname = $faker->firstName();
-            $newMessage->lastname = $faker->lastName(); 
-            $newMessage->email = $faker->email(); 
-            $newMessage->text = $faker->words(40, true); 
+            $newMessage->user_id = $varUser_id;
+            $newMessage->firstname = $varFirstname;
+            $newMessage->lastname = $varLastname;
+            $newMessage->email = $varEmail; 
+            $newMessage->text = $varText;
             $newMessage->save();
         }
+
+
     }
 }
--- database/seeds/ReviewsTableSeeder.php
+++ database/seeds/ReviewsTableSeeder.php
@@ -13,19 +13,87 @@ class ReviewsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        //array per compilare tabella recensioni
+        $goodArtist = [
+            'Eccellente',
+            'Ottimo'
+        ];
 
-        //Riempimento tabella recensioni tramite Faker
-        //ToDo: creare un array di recensioni verosimili o istruire meglio il faker
-        
+        $normalArtist = [
+            'Molto professionale',
+            'Professionale',
+            'Consiglio'
+        ];
+
+        $badArtist = [
+            'Poco professionale',
+            'Pensavo meglio',
+            'Sconsiglio'
+        ];
+
+        $goodTxt = [
+            'Ottima esperienza, molto professionale. Consiglio a tutti',
+            'Assolutamente perfetto: rapido, preciso, e soprattutto con un talento raro.',
+            'Tecnica perfetta, passione per la musica e per il proprio lavoro'
+        ];
+        $normalTxt = [
+            'Puntuale, preciso e professionale. Consiglio a tutti',
+            'Non è il top ma per il prezzo direi molto bene'
+        ];
+        $badTxt = [
+            'Poca professionalità, ci siamo trovati malissimo',
+            'Dice di saper suonare ma in realtà non è vero',
+            "Rapporto performance-prezzo pessimo. Sconsiglio"
+        ];
+
+        //compilazione tabella recensioni
         for ( $i = 0; $i < 60; $i++ ) {
-            $author = $faker->name();
+
+            //contatori array
+            $countGood=array_rand($goodArtist, 1);
+            $countNormal=array_rand($normalArtist, 1);
+            $countBad=array_rand($badArtist, 1);
+            $countGoodText=array_rand($goodTxt, 1);
+            $countNormalText=array_rand($normalTxt, 1);
+            $countbadText=array_rand($badTxt, 1);
+
+
+            //variabili seeder
+            $varUser_id = $faker-> numberBetween(1, 25);
+            $varName = $faker->name();
+            $varVote = $faker->numberBetween(0, 5);
+
+            switch ($varVote) {
+
+                case (0):
+                case (1):
+                case (2):
+                    $varTitle=$badArtist[$countBad];
+                    $varContent=$badTxt[$countbadText];
+                    break;
+
+                case (3):
+                    $varTitle=$normalArtist[$countNormal];
+                    $varContent=$normalTxt[$countNormalText];
+                    break;
+
+                case (4):
+                case (5):
+                    $varTitle=$goodArtist[$countGood];
+                    $varContent=$goodTxt[$countGoodText];
+                    break;
+            }
+
+            
+            //SEEDER
             $newReview = new Review();
-            $newReview->user_id = $faker->numberBetween(1, 20); //generazione casuale della foreign key in modo da avere un assegnazione per gli user creati con il seeder
-            $newReview->title = "Recensione di " . $author;
-            $newReview->author = $author;
-            $newReview->content = $faker->words(40, true);
-            $newReview->vote = $faker->numberBetween(0, 5);
+            $newReview->user_id = $varUser_id;
+            $newReview->author = $varName;
+            $newReview->vote = $varVote;
+            $newReview->title = $varTitle;
+            $newReview->content = $varContent;
             $newReview->save();
+            
         }
     }
 }
