8db006abf136fb482a44a2decad8a511913a1efc
Kio-dev-it <pas.petito@gmail.com>
2021-12-18 17:26:44 +0100

    Modificato formato date passate

--- app/Http/Controllers/Admin/SponsorshipController.php
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use App\Sponsorship;
 use App\User;
+use DateTime;
 
 class SponsorshipController extends Controller
 {
@@ -15,12 +17,28 @@ class SponsorshipController extends Controller
 
         $user = Auth::user();
 
-        $userSponsor = User::where('id', $user->id)->with('sponsorships')->get();
-        // dd($userSponsor);
-        $prova = $userSponsor[0]['sponsorships'];
-        // return response()->json([
-        //     'data' => $prova
-        // ]);
+        $userSponsor = User::where('id', $user->id)
+        ->with(['sponsorships' => function($query){
+            $query->orderBy('start_time', 'desc');
+        }])
+        // ->orderBy('start_time', 'asc')
+        ->get();
+        
+        foreach($userSponsor[0]['sponsorships'] as $spons){
+            $start_time = new DateTime($spons['pivot']['start_time']);
+            $spons['pivot']['start_time'] = date_format($start_time, "d-m-Y H:i");
+
+            $end_time = new DateTime($spons['pivot']['end_time']);
+            $spons['pivot']['end_time'] = date_format($end_time, "d-m-Y H:i");
+
+            $created_at = new DateTime($spons['pivot']['created_at']);
+            $spons['pivot']['created_at'] = date_format($created_at, "d-m-Y H:i");
+        };
+
+        $prova = $userSponsor[0]['sponsorships']; 
+
+        // return response()->json(['data' => $prova]);
+
         $sponsorships = Sponsorship::all();
         
         
