a180b0e068d8f846117df0115f689731bdfe8ba7
Kio-dev-it <pas.petito@gmail.com>
2021-12-18 06:55:10 +0100

    Modifica SponsorUserSeeder per avere sponsor prec

--- database/seeds/SponsorshipUserTableSeeder.php
+++ database/seeds/SponsorshipUserTableSeeder.php
@@ -14,22 +14,35 @@ class SponsorshipUserTableSeeder extends Seeder
     public function run()
     {   
         $arrayUser = [];
-        for( $i=1; $i<11; $i++) {
+        for( $i=1; $i<20; $i++) {
             $randomUser=rand(1, 25);
             $randomSponsor=rand(1,3);
-
+            $randomStart = rand(1,2);
             //settaggio giorni start e end
             date_default_timezone_set('Europe/Rome'); //fuso orario italiano
+
             $tmp1=date("Y-m-d H:i:s"); //data di oggi in una variabile temporanea 
             $dateMin = new DateTime($tmp1); // attribuzione la data di oggi a dateMin settandola con new DateTime
 
-            $dateTimeStamp = rand(2, 7); //numero randomico tra 2 e 7
-            $dateMax = date_modify($dateMin,  "+$dateTimeStamp days"); //imposto la dateMax modificando la tmp2, che ha il valore di dateMin, aggiungendo il numero casuale
-
-            
             $tmp2=date("Y-m-d H:i:s"); //data di oggi in una variabile temporanea 
             $endDateMin = new DateTime($tmp2); // attribuzione la data di oggi a dateMin settandola con new DateTime
-            $endDateMax = date_modify($endDateMin,  "+$dateTimeStamp days"); //imposto la dateMax modificando la tmp2, che ha il valore di dateMin, aggiungendo il numero casuale
+
+            $dateTimeStamp = rand(2, 7); //numero randomico tra 2 e 7
+            
+            switch($randomStart){
+                case(1):
+                    $dateMax = date_modify($dateMin,  "+$dateTimeStamp days"); //imposto la dateMax modificando la tmp2, che ha il valore di dateMin, aggiungendo il numero casuale
+                    $endDateMax = date_modify($endDateMin,  "+$dateTimeStamp days"); //imposto la dateMax modificando la tmp2, che ha il valore di dateMin, aggiungendo il numero casuale
+                    break;
+    
+                case(2):
+                    $dateMax = date_modify($dateMin,  "-$dateTimeStamp days"); //imposto la dateMax modificando la tmp2, che ha il valore di dateMin, aggiungendo il numero casuale
+                    $endDateMax = date_modify($endDateMin,  "-$dateTimeStamp days"); //imposto la dateMax modificando la tmp2, che ha il valore di dateMin, aggiungendo il numero casuale
+                    break;
+    
+            }
+
+            
             
             switch ($randomSponsor) {
 
