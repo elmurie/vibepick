7076134b1fe689cf85207cbc790ab887c444adad
elmurie <elmurie@gmail.com>
2021-12-19 12:54:41 +0100

    aggiunte statistiche  per mese

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -9,6 +9,8 @@ use App\User;
 use App\Message;
 use App\Review;
 use App\Chart;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class StatisticsController extends Controller
 {
@@ -24,12 +26,93 @@ class StatisticsController extends Controller
             $voti[] = $value->vote;
         };
         $chart['voti'] = $voti;
+
+
         $date = [];
         foreach ($reviews as $value){
             $date[] = date_format($value->created_at, "d-m-Y H:i:s");
         };
         $chart['date'] = $date;
 
-        return view('admin.statistics.index', compact('user', 'messages', 'reviews', 'chart'));
+        //count review diviso per mese 
+        $reviewsMonth = new Chart;
+        $monthsArray = [
+            0 => 'Gennaio', 
+            1 => 'Febbraio', 
+            2 => 'Marzo', 
+            3 => 'Aprile', 
+            4 => 'Maggio', 
+            5 => 'Giugno', 
+            6 => 'Luglio', 
+            7 => 'Agosto', 
+            8 => 'Settembre', 
+            9 => 'Ottobre', 
+            10 => 'Novembre', 
+            11 => 'Dicembre'
+       ];
+
+        $monthly_uploaded_product = DB::table('reviews')
+        ->where('user_id', $user->id)
+        ->select(DB::raw('count(id) as total'), DB::raw('MONTH(created_at) as month'))
+        ->groupBy('month')
+        ->get();
+
+        $year = [
+            0 => 0,
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+            6 => 0,
+            7 => 0,
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => 0,
+        
+        ];//initialize all months to 0
+
+        foreach($monthly_uploaded_product as $key => $value) {
+        $year[$value->month-1] = $value->total;//update each month with the total value
+        };
+
+        $reviewsMonth['mesi'] = $monthsArray;
+        $reviewsMonth['tot'] = $year;
+
+
+        // count messaggi diviso per mese 
+        $messagesMonth = new Chart;
+        $monthly_messages = DB::table('messages')
+        ->where('user_id', $user->id)
+        ->select(DB::raw('count(id) as total'), DB::raw('MONTH(created_at) as month'))
+        ->groupBy('month')
+        ->get();
+
+        $year_messages = [
+            0 => 0,
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+            6 => 0,
+            7 => 0,
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => 0,
+        
+        ];//initialize all months to 0
+
+        foreach($monthly_messages as $key => $value) {
+        $year_messages[$value->month-1] = $value->total;//update each month with the total value
+        };
+
+        $messagesMonth['mesi'] = $monthsArray;
+        $messagesMonth['tot'] = $year_messages;
+
+
+        return view('admin.statistics.index', compact('user', 'messages', 'reviews', 'chart', 'reviewsMonth', 'messagesMonth'));
     }
 }
