43cfcddc0a0c48b83371baa08627d3c86c18e084
Kio-dev-it <pas.petito@gmail.com>
2021-12-19 23:24:44 +0100

    Aggiunti dati sponsor attiva in edit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -122,10 +122,57 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        if( Auth::user() ) {
-            $user = Auth::user();
+        if( $user = Auth::user() ) {
+            // $user = Auth::user();
             $instruments = Instrument::all();
-            return view('admin.edit', compact('instruments', 'user'));
+            
+            $userSelected = User::where('id', $user->id)
+            ->with('sponsorships')
+            ->get();
+
+            $sponsorships = $userSelected['0']['sponsorships'];
+
+            //Recupero della data odierna
+            date_default_timezone_set('Europe/Rome');
+            $nowD = date("Y-m-d H:i:s");
+
+            $nowDate = new DateTime($nowD);
+
+            $sponsLength = count($sponsorships);
+            
+            
+            for($i = 0; $i < $sponsLength; $i++){
+                $start_time = new DateTime($sponsorships[$i]['pivot']['start_time']);
+                $end_time = new DateTime($sponsorships[$i]['pivot']['end_time']);
+                $created_at = $sponsorships[$i]['pivot']['created_at'];
+                $created_at = $created_at->format("d-m-Y h:i");
+
+                
+                //Assegnazione di una classe per l'eventuale sponsorizzazione attiva
+                if(!($start_time < $nowDate && $end_time > $nowDate)){
+                    unset($sponsorships[$i]);
+                }else{
+                    $sponsorships[$i]['pivot']['start_time'] = date_format($start_time, "d-m-Y H:i");
+                    $sponsorships[$i]['pivot']['end_time'] = date_format($end_time, "d-m-Y H:i");
+                    $sponsorships[$i]['pivot']['sale_time'] = $created_at;
+                    $conto = $i;
+                }
+                
+                //Formattazione delle date per la visione a video
+            }
+
+
+            $user = $userSelected[0];
+            if(count($userSelected[0]['sponsorships']) > 0){
+                $sponsorship = $userSelected[0]['sponsorships'][$conto];
+                $userSelected[0]['is_sponsored'] = true;
+
+            } else{
+                $userSelected[0]['is_sponsored'] = false;
+                $sponsorship = $userSelected[0]['sponsorships'];
+
+            }
+            return view('admin.edit', compact('instruments', 'user', 'sponsorship'));
         }    
         abort("403");
     }
